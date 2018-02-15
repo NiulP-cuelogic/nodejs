98fb04475dc4ef3a1fe9af744d472bfe3395f0c1
Niul Panvalkar <niul.panvalkar@cuelogic.com>
2018-02-15 16:00:53 +0530

    tried examples on promisess and file handling

---
+++ day 2/myApp2/promises.js
@@ -0,0 +1,32 @@
+var prompt = require('prompt');
+prompt.start();
+let myCalender = new Promise(function(resolve,reject){
+
+    prompt.get(['salary','day'],function(err,result){
+        var x = result.day;
+        if(x>=1 && x<=5){
+            var z = result.salary/(35-x-30);
+            // console.log(z);
+            isOkay = true;
+            resolve(z);
+        }
+        else if(x>5 && x<=30){
+            var z = result.salary/35-x;
+            // console.log(z);
+            isOkay = true;
+            resolve(z);
+        }
+        else{
+            reject('invalid day');
+            isOkay = false;
+        }
+    })
+    
+})
+
+
+myCalender.then(function(fromResolve){
+    console.log('the answer is ' + fromResolve);
+}).catch(function(fromReject){
+    console.log('invalid day ..');
+})
